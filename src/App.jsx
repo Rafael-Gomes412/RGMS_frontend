c2d8c9e1ba97d5214902ba15650ad57eb50ce7b7
Rafael-Gomes412 <rafaelgomesm@outlook.fr>
2026-06-22 12:52:42 +0200

    Scrooll

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import CMen from './pages/C_men'
 import CWomen from './pages/C_women'
@@ -16,8 +17,6 @@ import Contact from './pages/Contact'
 import FAQ from './pages/FAQ'
 import Mentions from './pages/Mentions'
 import Community from './pages/Community'
-
-// AJOUT DES IMPORTS POUR LE BLOG
 import Blog from './pages/Blog'
 import BlogDetail from './pages/BlogDetail'
 
@@ -28,6 +27,7 @@ function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Navbar transparent={isTransparent} />
       <div className={isTransparent ? '' : 'pt-20'}>
         <Routes>
@@ -41,13 +41,11 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
-          <Route path="/about" element={<About />} /> 
+          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/faq" element={<FAQ />} />
           <Route path="/mentions" element={<Mentions />} />
           <Route path="/community" element={<Community />} />
-          
-         
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:slug" element={<BlogDetail />} />
         </Routes>
